191428d4b61fe0baea19f9540073403535a7414b
Taras Kotsur <>
2021-05-11 19:45:33 +0300

    Refactored project structure

--- Movies/Protocols/Identifiable.swift
+++
@@ -1,9 +0,0 @@
-// Identifiable.swift
-// Copyright © Taras Kotsur. All rights reserved.
-
-import Foundation
-
-protocol Identifiable {
-    associatedtype IdentifiableType
-    static var identifire: IdentifiableType { get }
-}
